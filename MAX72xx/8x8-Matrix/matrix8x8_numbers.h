87f3005377dfe40c4248e8bc0d40e63770c02f96
digitalmuddler <digitalmuddler@users.noreply.github.com>
2015-09-10 15:30:49 -0400

    Create matrix8x8_numbers.h

---
+++ MAX72xx/8x8-Matrix/matrix8x8_numbers.h
@@ -0,0 +1,15 @@
+// set numbers for 8x8 matrix
+// uses setRow from LedControl.h and LedControl.cpp files
+// leftmost and rightmost columns are OFF
+// top row is OFF
+
+byte n0[8] = {B00000000,
+byte n1[8] = {B00000000,
+byte n2[8] = {B00000000,
+byte n3[8] = {B00000000,
+byte n4[8] = {B00000000,
+byte n5[8] = {B00000000,
+byte n6[8] = {B00000000,
+byte n7[8] = {B00000000,
+byte n8[8] = {B00000000,
+byte n9[8] = {B00000000,
