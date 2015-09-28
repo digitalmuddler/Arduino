3189f1a2f4fb419246c59b6ebd3ea65a0fcc275c
digitalmuddler <digitalmuddler@users.noreply.github.com>
2015-09-28 11:02:33 -0400

    Update matrix8x8_letters.h
    separated setColumn and setRow letter types

--- MAX72xx/8x8-Matrix/matrix8x8_letters.h
+++ MAX72xx/8x8-Matrix/matrix8x8_letters.h
@@ -1,31 +1,68 @@
 // set letters for 8x8 matrix
-// uses setRow from LedControl.h and LedControl.cpp files
+// uses setRow and setColumn from LedControl.h and LedControl.cpp files
+
+// setColumn - CAPITAL LETTERS
 // leftmost and rightmost columns are OFF
 // top row is OFF
 
-byte A[8] = {B00000000,B01111111,B10001000,B10001000,B10001000,B01111111,B00000000,B00000000};
-byte B[8] = {B00000000,B01111111,
-byte C[8] = {B00000000,
-byte D[8] = {B00000000,B01111111,
-byte E[8] = {B00000000,B01111111,
-byte F[8] = {B00000000,B01111111,
-byte G[8] = {B00000000,
-byte H[8] = {B00000000,B01111111,B00001000,B00001000,B00001000,B00001000,B01111111,B00000000};
-byte I[8] = {B00000000,
-byte J[8] = {B00000000,
-byte K[8] = {B00000000,B01111111,
-byte L[8] = {B00000000,B01111111,
-byte M[8] = {B00000000,B01111111,
-byte N[8] = {B00000000,B01111111,
-byte O[8] = {B00000000,
-byte P[8] = {B00000000,B01111111,
-byte Q[8] = {B00000000,
-byte R[8] = {B00000000,B01111111,
-byte S[8] = {B00000000,
-byte T[8] = {B00000000,
-byte U[8] = {B00000000,
-byte V[8] = {B00000000,
-byte W[8] = {B00000000,
-byte X[8] = {B00000000,
-byte Y[8] = {B00000000,
-byte Z[8] = {B00000000,
+byte cA[8] = {B00000000,B01111111,B10001000,B10001000,B10001000,B01111111,B00000000,B00000000};
+byte cB[8] = {B00000000,B01111111,
+byte cC[8] = {B00000000,
+byte cD[8] = {B00000000,B01111111,
+byte cE[8] = {B00000000,B01111111,
+byte cF[8] = {B00000000,B01111111,
+byte cG[8] = {B00000000,
+byte cH[8] = {B00000000,B01111111,B00001000,B00001000,B00001000,B00001000,B01111111,B00000000};
+byte cI[8] = {B00000000,
+byte cJ[8] = {B00000000,
+byte cK[8] = {B00000000,B01111111,
+byte cL[8] = {B00000000,B01111111,
+byte cM[8] = {B00000000,B01111111,
+byte cN[8] = {B00000000,B01111111,
+byte cO[8] = {B00000000,
+byte cP[8] = {B00000000,B01111111,
+byte cQ[8] = {B00000000,
+byte cR[8] = {B00000000,B01111111,
+byte cS[8] = {B00000000,
+byte cT[8] = {B00000000,
+byte cU[8] = {B00000000,
+byte cV[8] = {B00000000,
+byte cW[8] = {B00000000,
+byte cX[8] = {B00000000,
+byte cY[8] = {B00000000,
+byte cZ[8] = {B00000000,
+
+// setColumn - LOWER CASE LETTERS
+// leftmost and rightmost columns are OFF
+// top row is OFF
+
+byte ca[8] = {
+
+
+// setRow 
+byte rA[8] = {
+byte rB[8] = {
+byte rC[8] = {
+byte rD[8] = {
+byte rE[8] = {
+byte rF[8] = {
+byte rG[8] = {
+byte rH[8] = {
+byte rI[8] = {
+byte rJ[8] = {
+byte rK[8] = {
+byte rL[8] = {
+byte rM[8] = {
+byte rN[8] = {
+byte rO[8] = {
+byte rP[8] = {
+byte rQ[8] = {
+byte rR[8] = {
+byte rS[8] = {
+byte rT[8] = {
+byte rU[8] = {
+byte rV[8] = {
+byte rW[8] = {
+byte rX[8] = {
+byte rY[8] = {
+byte rZ[8] = {
