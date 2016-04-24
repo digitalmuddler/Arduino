58462870d8638f7faac6ddb35ecce564e66897b5
digitalmuddler <digitalmuddler@users.noreply.github.com>
2016-04-24 00:34:59 -0400

    Update launch.ino
    Added LED initialization; need additional LED labels and pin outs

--- Model-Rocket-Launcher/launch.ino
+++ Model-Rocket-Launcher/launch.ino
@@ -4,12 +4,43 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+// initialize LEDs
+int LED_AuxPower = TBD;       // pin # TBD
+int LED_SafetyEngaged = TBD;  // pin # TBD
+int LED_SafetyEngaged = TBD;  // pin # TBD
+//int LED???
+int LED_IgnitionArmed = TBD;  // pin # TBD
+//int LED???
+int LED_ReadyForLaunch = TBD; // pin # TBD
+//int LED???
+int LED_LiftOff = TBD;        // pin # TBD
+int LED_Prelaunch = TBD;      // pin # TBD
+int LED_Launch = TBD;         // pin # TBD
+
 void setup() {
   //initialize the LCD
   lcd.begin(16, 2);
 
+  //initialize digital pins on the Arduino as output
+  pinMode(LED_AuxPower, OUTPUT);
+  pinMode(LED_SafetyEngaged, OUTPUT);  
+  pinMode(LED_SafetyEngaged, OUTPUT);
+  //pinMode(  ?????
+  pinMode(LED_IgnitionArmed, OUTPUT);
+  //int LED???
+  pinMode(LED_ReadyForLaunch, OUTPUT);
+  //int LED???
+  pinMode(LED_LiftOff, OUTPUT);
+  pinMode(LED_Prelaunch, OUTPUT);
+  pinMode(LED_Launch, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_AuxPower, HIGH);     // turns on LED
+  
   //if power on/connection off/launch off
   lcd.print("System Standby");
+  lcd.setCursor("Review Safety");
   delay(5000); //this line goes away when igniter is connected
  
   //power on/igniter connected/launch off
@@ -44,12 +75,4 @@ void setup() {
   
   //lcd.setCursor(0,1);
   //lcd.print("Countdown:");
-}
-
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  //lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  // lcd.print(millis() / 1000);  
 }
